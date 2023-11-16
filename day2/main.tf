e01e51aee50d80f324c10b21438fca5c54158d0b
pavan_varada <pavankumar.varada1@gmail.com>
2023-11-16 11:30:46 +0000

    here day2 created local state file

---
+++ day2/main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "gtg-01" {
+  name                      = local.resource_group_name
+  location                  = var.location
+}
+
+resource "azurerm_storage_account" "gtg-01" {
+  name                      = local.storage_account_name
+  resource_group_name       = azurerm_resource_group.gtg-01.name
+  location                  = var.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+}
